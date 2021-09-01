3147841a9fff140e6f93f9f32aad75430c26c2c4
atulkumar9888 <60936934+atulkumar9888@users.noreply.github.com>
2021-09-01 19:29:28 +0530

    Update Restaurant.java

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -92,17 +92,7 @@ public class Restaurant {
         return name;
     }
 
-    //Method to compute Cost of all the Menu Items in a restaurant
-    public int getTotalCostOfItems(List<Item> selectedItems){
-        int totalCost = 0;
-        for(Item item: selectedItems) {
-
-            totalCost = totalCost + item.getPrice();
-        }
-        return  totalCost;
-
-
-    }
+    
 
 
 }
